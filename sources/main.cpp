7c39574c60e34d0e619f05e334c1889d8ddad5d3
배성훈 <dan990429@gmail.com>
2024-05-02 16:23:50 +0900

    feat: Add memory usage calculation and display

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,66 +1,68 @@
+#include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <thread>
 
 struct MemInfo {
-  float   Active;
-  float   ActiveAnon;
-  float   ActiveFile;
-  float   AnonHugePages;
-  float   AnonPages;
-  float   Bounce;
-  float   Buffers;
-  float   Cached;
-  float   CmaFree;
-  float   CmaTotal;
-  float   CommitLimit; 
-  float   Committed_AS; 
-  float   DirectMap1G; 
-  float   DirectMap2M;
-  float   DirectMap4k;
-  float   Dirty; 
-  float   FileHugePages; 
-  float   FilePmdMapped; 
-  float   HardwareCorrupted; 
-  float   HugePages_Free; 
-  float   HugePages_Rsvd;
-  float   HugePages_Surp;
-  float   HugePages_Total; 
-  float   Hugepagesize; 
-  float   Hugetlb; 
-  float   Inactive; 
-  float   InactiveAnon; 
-  float   InactiveFile; 
-  float   KReclaimable; 
-  float   KernelStack;
-  float   Mapped; 
-  float   MemAvailable; 
-  float   MemFree; 
-  float   MemTotal; 
-  float   Mlocked; 
-  float   NFS_Unstable; 
-  float   PageTables; 
-  float   Percpu; 
-  float   SReclaimable; 
-  float   SUnreclaim; 
-  float   SecPageTables; 
-  float   Shmem; 
-  float   ShmemHugePages; 
-  float   ShmemPmdMapped; 
-  float   Slab; 
-  float   SwapCached; 
-  float   SwapFree; 
-  float   SwapTotal ;
-  float   Unaccepted;
-  float   Unevictable;
-  float   VmallocChunk;
-  float   VmallocTotal; 
-  float   VmallocUsed;
-  float   Writeback; 
-  float   WritebackTmp;
-  float   Zswap;
-  float   Zswapped;
-  float   Process;
+  float Active;
+  float ActiveAnon;
+  float ActiveFile;
+  float AnonHugePages;
+  float AnonPages;
+  float Bounce;
+  float Buffers;
+  float Cached;
+  float CmaFree;
+  float CmaTotal;
+  float CommitLimit;
+  float Committed_AS;
+  float DirectMap1G;
+  float DirectMap2M;
+  float DirectMap4k;
+  float Dirty;
+  float FileHugePages;
+  float FilePmdMapped;
+  float HardwareCorrupted;
+  float HugePages_Free;
+  float HugePages_Rsvd;
+  float HugePages_Surp;
+  float HugePages_Total;
+  float Hugepagesize;
+  float Hugetlb;
+  float Inactive;
+  float InactiveAnon;
+  float InactiveFile;
+  float KReclaimable;
+  float KernelStack;
+  float Mapped;
+  float MemAvailable;
+  float MemFree;
+  float MemTotal;
+  float Mlocked;
+  float NFS_Unstable;
+  float PageTables;
+  float Percpu;
+  float SReclaimable;
+  float SUnreclaim;
+  float SecPageTables;
+  float Shmem;
+  float ShmemHugePages;
+  float ShmemPmdMapped;
+  float Slab;
+  float SwapCached;
+  float SwapFree;
+  float SwapTotal;
+  float Unaccepted;
+  float Unevictable;
+  float VmallocChunk;
+  float VmallocTotal;
+  float VmallocUsed;
+  float Writeback;
+  float WritebackTmp;
+  float Zswap;
+  float Zswapped;
+  float Process;
 };
 
 void trim(std::string &s) {
@@ -74,32 +76,42 @@ void trim(std::string &s) {
   }
 }
 int main() {
-  std::ifstream file("/proc/meminfo");
-  if (!file) {
-    std::cerr << "Cannot open /proc/meminfo" << std::endl;
-    return 1;
-  }
-
-  std::map<std::string, std::string> meminfo;
-  std::string line;
-  while (std::getline(file, line)) {
-    std::string key = line.substr(0, line.find(":"));
-    std::string value = line.substr(line.find(":") + 1);
-    trim(value);
-    meminfo[key] = value;
-  }
-
-  struct MemInfo meminform;
-  for (const auto& pair : meminfo) {
-    if (pair.first == "MemTotal") {
-      meminform.MemTotal = std::stoi(pair.second);
-    }
-    if (pair.first == "MemFree") {
-      meminform.MemFree = std::stoi(pair.second);
+  while (true) {
+    std::ifstream file("/proc/meminfo");
+    if (!file) {
+      std::cerr << "Cannot open /proc/meminfo" << std::endl;
+      return 1;
     }
 
-  }
-  std::cout <<  meminform.MemFree / meminform.MemTotal << std::endl;
+    std::map<std::string, std::string> meminfo;
+    std::string line;
+    while (std::getline(file, line)) {
+      std::string key = line.substr(0, line.find(":"));
+      std::string value = line.substr(line.find(":") + 1);
+      trim(value);
+      meminfo[key] = value;
+    }
 
+    struct MemInfo meminform;
+    for (const auto &pair : meminfo) {
+      if (pair.first == "MemTotal") {
+        meminform.MemTotal = std::stoi(pair.second);
+      }
+      if (pair.first == "MemFree") {
+        meminform.MemFree = std::stoi(pair.second);
+      }
+      if (pair.first == "Cached") {
+        meminform.Cached = std::stoi(pair.second);
+      }
+    }
+
+    // get memory using but not in cached
+    auto memUsed = (meminform.MemTotal - (meminform.MemFree + meminform.Cached)) / 1024 / 1024;
+    auto memUsedWithCache = (meminform.MemTotal - meminform.MemFree) / 1024 / 1024;
+
+    printf("%.2f/%.2f/%.2f\n", memUsed,  meminform.Cached / 1024 / 1024, memUsedWithCache);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
   return 0;
 }
